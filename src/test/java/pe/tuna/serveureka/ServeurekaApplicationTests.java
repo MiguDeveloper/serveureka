85477cdc186694d500b11309e8fef6c3d513b049
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-10-28 22:22:22 -0500

    configuracion basica eureka server

---
+++ src/test/java/pe/tuna/serveureka/ServeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.tuna.serveureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
